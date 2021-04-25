e0027d9acc082748a12041d0c54a478b30c848c7
Wachukwu Emmanuel <immacurte1@gmail.com>
2021-04-25 03:08:53 +0100

    Middleware file with isLoggedI function

---
+++ middleware.js
@@ -0,0 +1,9 @@
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.flash('error', 'You must be signed in first!')
+        return res.redirect('/login')
+    }
+    else {
+        next()
+    }
+};
