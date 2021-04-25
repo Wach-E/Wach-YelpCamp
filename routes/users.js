34fb81adb6af4fe7d9c2c2cf7dd4e41f41fc8b10
Wachukwu Emmanuel <immacurte1@gmail.com>
2021-04-25 05:13:27 +0100

    Added returnBehavoir to route and login method to register route

--- routes/users.js
+++ routes/users.js
@@ -8,14 +8,17 @@ router.get('/register', (req, res) => {
     res.render('users/register')
 })
 
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password)
-        console.log(registeredUser);
-        req.flash('success', 'Welcome to YelpCamp!');
-        res.redirect('/campgrounds')
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to YelpCamp!');
+            res.redirect('/campgrounds')
+        })
+
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('/register')
@@ -29,8 +32,16 @@ router.get('/login', (req, res) => {
 router.post('/login',
     passport.authenticate('local'),
     function (req, res) {
+        const redirectUrl = req.session.returnTo || '/campgrounds'
+        delete req.session.returnTo
         req.flash('success', 'Welcome back to YelpCamp!')
-        res.redirect('/campgrounds');
+        res.redirect(redirectUrl);
     });
+
+router.get('/logout', (req, res) => {
+    req.logout()
+    req.flash('success', 'Thank you for using YelpCamp, Goodbye!')
+    res.redirect('/campgrounds')
+})
 
 module.exports = router;
