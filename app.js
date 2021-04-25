14be8ea290b7bf2e6978dfe134b76302fc20add1
Wachukwu Emmanuel <immacurte1@gmail.com>
2021-04-25 03:34:46 +0100

    Modified

--- app.js
+++ app.js
@@ -45,7 +45,6 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
