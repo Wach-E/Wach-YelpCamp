004d0c83674fca2f2ee67b2456eeb2f72edfcd92
Wachukwu Emmanuel <immacurte1@gmail.com>
2021-04-29 04:03:17 +0100

    Added author field to review schema

--- models/review.js
+++ models/review.js
@@ -3,7 +3,11 @@ const Schema = mongoose.Schema;
 
 const ReviewSchema = new Schema({
     body: String,
-    rating: Number
+    rating: Number,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
 });
 
 module.exports = mongoose.model('Review', ReviewSchema);
