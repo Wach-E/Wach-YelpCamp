8ac0e68d2f921362899781ca6c4e708e90189f66
Wachukwu Emmanuel <immacurte1@gmail.com>
2021-05-08 08:14:51 +0100

    Revamped with ImageSchema and process virtual

--- models/campground.js
+++ models/campground.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/h_100,w_100')
+});
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
