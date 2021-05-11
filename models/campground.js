1ffa2b45607417b012800958ab1016dedc3f4fca
Wachukwu Emmanuel <immacurte1@gmail.com>
2021-05-11 04:30:04 +0100

    Added geometry field

--- models/campground.js
+++ models/campground.js
@@ -16,6 +16,17 @@ const CampgroundSchema = new Schema({
     price: Number,
     description: String,
     location: String,
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
     author: {
         type: Schema.Types.ObjectId,
         ref: 'User'
