82403d48f64cade57e887fd315d658f7c6ed77ca
Wachukwu Emmanuel <immacurte1@gmail.com>
2021-05-12 09:29:56 +0100

    Added map logic

---
+++ public/javascripts/showMap.js
@@ -0,0 +1,20 @@
+const camp = JSON.parse(campground);
+// Create a map
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
+    center: camp.geometry.coordinates, // starting position [lng, lat]
+    zoom: 5 // starting zoom
+});
+
+// Create a marker and add it to the map.
+new mapboxgl.Marker()
+    .setLngLat(camp.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h4>${camp.title}</h4><p>${camp.location}</p>`
+            )
+    )
+    .addTo(map);
